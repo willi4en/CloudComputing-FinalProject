7d5e793098657318cf3985bbf021a1e6d542a5ab
willi4en <willi4en@mail.uc.edu>
2023-04-22 20:25:47 -0400

    Update

--- app.py
+++ app.py
@@ -67,7 +67,8 @@ def get_hshd_attrs(hshd_list):
     data = []
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute("SELECT * FROM households WHERE HSHD_NUM IN ({})".format(', '.join(['?']*len(hshd_list))), hshd_list)
+    cur.execute("SELECT * FROM households WHERE HSHD_NUM IN ({})".format(
+        ', '.join(['?']*len(hshd_list))), hshd_list)
     household_data = cur.fetchall()
     for household in household_data:
         table_row = {
@@ -80,7 +81,7 @@ def get_hshd_attrs(hshd_list):
             'HSHD_COMPOSITION': household[6],
             'HH_SIZE': household[7],
             'CHILDREN': household[8]
-            }
+        }
         data.append(table_row)
     data = pd.DataFrame(data)
     if not data.empty:
@@ -90,7 +91,8 @@ def get_hshd_attrs(hshd_list):
         print(unique_counts.index)
         for col in unique_counts.index:
             most_common[col] = data[col].value_counts().idxmax()
-        data = pd.DataFrame(most_common)    
+        print(most_common)
+        data = pd.DataFrame(most_common)
     else:
         data = pd.DataFrame()
     return data
